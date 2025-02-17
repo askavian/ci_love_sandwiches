19b4be61a4362c793e1172854682d522e24169fe
askavian <malte.boettcher@live.com>
2025-02-17 21:15:27 +0100

    feat: data validation for sales data input

--- run.py
+++ run.py
@@ -16,9 +16,9 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('ci_love_sandwiches')
 
-#sales = SHEET.worksheet('sales')                               #  test if API is working
-#data = sales.get_all_values()                                  #  test if API is working
-#print(data)                                                    #  test if API is working
+#sales = SHEET.worksheet('sales')                               #  TEST: if API is working
+#data = sales.get_all_values()                                  #  TEST: if API is working
+#print(data)                                                    #  TEST: if API is working
 
 def get_sales_data():
     """
@@ -26,9 +26,27 @@ def get_sales_data():
     """
     print("Please enter the sales data from the last market.")
     print("Data should be six numbers, seperated by commas.")
-    print("Example: 10,20,30,40,50,60\n")                       #  \n = New Line Character
+    print("Example: 10,20,30,40,50,60\n")                       #  "\n" = New Line Character
 
-    data_str = input("Enter your data here: ")
-    print(f"The data provided is {data_str}")                   #  f string for interpolation of values 
+    data_str = input("Enter your data here: ")                  #  input MUST BE exactly 6 Int
+    #print(f"The data provided is {data_str}")                  #  TEST: "f" string for interpolation of values 
+
+    sales_data = data_str.split(",")                            #  REMOVES string-"," and ADDS list-"," creates "data_str" list
+    validate_data(sales_data)                                   #  calls "validate_data" function from inside "get_sales_data" function
+
+
+def validate_data(values):                                      #  validates data function ("values" is relative to where function is called from)
+    """
+    Inside the try, converts all string values into integers.
+    Raises ValueError if strings cannot be converted into int,
+    or if there aren't exactly 6 values.
+    """
+    try:
+        if len(values) != 6:                                    #  if the values (list) IS NOT exactly 6 numbers     
+            raise ValueError(                                   #  raises ValueError and shows lenght given            
+                f"Exactly 6 values required, you provided {len(values)}"
+            ) 
+    except ValueError as e:                                     #  "e" is Python shorthand for ERROR    
+        print(f"Invalid data: {e}, please try again.\n")        #  combines "raise ValueError" into statement wie "e" keyword                 
 
 get_sales_data()
